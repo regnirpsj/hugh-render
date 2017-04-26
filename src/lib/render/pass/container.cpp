30e97d95677a857fd6f0035833353ff9e1ad0d09
jan p springer <jsd@igroup.org>
2017-04-26 10:05:07 +0100

    fixed: support/io usage

--- src/lib/render/pass/container.cpp
+++ src/lib/render/pass/container.cpp
@@ -2,7 +2,7 @@
 
 /**************************************************************************************************/
 /*                                                                                                */
-/* Copyright (C) 2016 University of Hull                                                          */
+/* Copyright (C) 2016-2017 University of Hull                                                     */
 /*                                                                                                */
 /**************************************************************************************************/
 /*                                                                                                */
@@ -23,7 +23,7 @@
 
 // includes, project
 
-#include <hugh/support/io_utils.hpp>
+#include <hugh/support/io.hpp>
 
 #define HUGH_USE_TRACE
 #undef HUGH_USE_TRACE
--- src/lib/render/window/base.cpp
+++ src/lib/render/window/base.cpp
@@ -2,7 +2,7 @@
 
 /**************************************************************************************************/
 /*                                                                                                */
-/* Copyright (C) 2016 University of Hull                                                          */
+/* Copyright (C) 2016-2017 University of Hull                                                     */
 /*                                                                                                */
 /**************************************************************************************************/
 /*                                                                                                */
@@ -25,7 +25,7 @@
 // includes, project
 
 #include <hugh/render/pass/base.hpp>
-#include <hugh/support/io_utils.hpp>
+#include <hugh/support/io.hpp>
 
 #define HUGH_USE_TRACE
 #undef HUGH_USE_TRACE
